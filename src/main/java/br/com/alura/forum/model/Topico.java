83f6e696b9ebb33c07f4e0056cd0ba15419f6d74
paulamilani <paula.millanii@gmail.com>
2020-07-28 19:28:41 -0300

    adjust

--- src/main/java/br/com/alura/forum/model/Topico.java
+++ src/main/java/br/com/alura/forum/model/Topico.java
@@ -1,19 +1,28 @@
 package br.com.alura.forum.model;
 
-import java.time.LocalDateTime;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Topico {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String mensagem;
 	//private LocalDateTime dataCriacao = LocalDateTime.now();
 	private String dataCriacao;
+	@Enumerated(EnumType.STRING)
 	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+	@ManyToOne
 	private Usuario autor;
+	@ManyToOne
 	private Curso curso;
+	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
 
 	public Topico(String titulo, String mensagem, Curso curso) {
